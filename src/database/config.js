9e8a1320ef787945c8caae1dd7f128562c7dd239
Ed Angelis <ed4ngelis@gmail.com>
2026-04-14 17:55:09 -0300

    feat: add database configuration file for development environment

---
+++ src/database/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'movies',
+    host:     process.env.DB_HOST || 'localhost',
+    port:     Number(process.env.DB_PORT) || 3306,
+    dialect:  'mysql',
+  },
+};
